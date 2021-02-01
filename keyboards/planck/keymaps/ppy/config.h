a5d8d1d738622474cb03aedc832d6f86dac17640
Dean Herbert <pe@ppy.sh>
2021-02-01 12:43:12 +0900

    Change leader key from backslash to quote

--- keyboards/planck/keymaps/ppy/config.h
+++ keyboards/planck/keymaps/ppy/config.h
@@ -3,6 +3,7 @@
 #define NO_DEBUG
 #define NO_PRINT
 
+// todo: fix key fuckery
 #define TAPPING_TERM 10
 #define RETRO_TAPPING
 
--- keyboards/planck/keymaps/ppy/keymap.c
+++ keyboards/planck/keymaps/ppy/keymap.c
@@ -23,7 +23,7 @@ enum planck_layers {
   _RAISE,
   _ADJUST,
   _VIM,
-  _VINSERT
+  _VINSERT,
 };
 
 enum planck_keycodes {
@@ -36,8 +36,8 @@ enum planck_keycodes {
 #define RAISE MO(_RAISE)
 #define CTRL_ESC MT(MOD_LCTL, KC_ESC)
 #define SFT_ENT MT(MOD_RSFT, KC_ENT)
-#define RAISE_BS LT(RAISE, KC_BSLS)
-#define VIM_BS LT(MO(_VIM), KC_BSLS)
+#define RAISE_BS LT(RAISE, KC_QUOT)
+#define VIM_BS LT(MO(_VIM), KC_QUOT)
 
 uint8_t vim_cmd_layer(void) {
     return _VIM;
@@ -48,12 +48,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT_planck_grid(
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     CTRL_ESC,KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_ENT,
-    KC_MEH,  KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  VIM_BS,  RAISE_BS,KC_DOWN, KC_UP,   KC_RGHT
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
+    KC_MEH,  KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  VIM_START,RAISE_BS,KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 [_LOWER] = LAYOUT_planck_grid(
-    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+    KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
     _______, _______, _______, _______, _______, _______, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______,_______, _______, _______, _______, _______
@@ -94,6 +94,23 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   if(!process_record_vimlayer(keycode, record)) return false;
 
   switch (keycode) {
+      case KC_BSPC:
+          if (record->event.pressed) {
+              if (get_mods() & MOD_BIT(KC_LCTL))
+              {
+                  unregister_code(KC_LCTL);
+
+                  register_code(KC_LALT);
+                  tap_code16(KC_BSPC);
+                  unregister_code(KC_LALT);
+
+                  register_code(KC_LCTL);
+                  return false;
+              }
+          }
+          return true;
+          break;
+
     case QWERTY:
       if (record->event.pressed) {
         set_single_persistent_default_layer(_QWERTY);
