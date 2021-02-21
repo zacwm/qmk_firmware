8db286beff553c5e8e246f715f7b3c83d9e929c3
Dean Herbert <pe@ppy.sh>
2021-02-22 00:22:32 +0900

    Just some formatting

--- keyboards/planck/keymaps/ppy/keymap.c
+++ keyboards/planck/keymaps/ppy/keymap.c
@@ -44,9 +44,7 @@ float song_kvm_setting[][2] = SONG(S__NOTE(_C5),S__NOTE(_C6),S__NOTE(_C7));
 float song_kvm_0[][2] = SONG(S__NOTE(_C5),S__NOTE(_C5),S__NOTE(_C5),S__NOTE(_C5));
 float song_kvm_1[][2] = SONG(S__NOTE(_G5),S__NOTE(_G5),S__NOTE(_G5),S__NOTE(_G5));
 
-uint8_t vim_cmd_layer(void) {
-    return _VIM;
-}
+uint8_t vim_cmd_layer(void) { return _VIM; }
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -54,34 +52,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
             CTRL_ESC,KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
             KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SFT_ENT,
-            KC_MEH,  KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   VIM_START,KVM_SWT,KC_UP,   KC_RGHT
-            ),
+            KC_MEH,  KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   VIM_START,KVM_SWT,KC_UP,   KC_RGHT),
 
     [_LOWER] = LAYOUT_planck_grid(
             KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
             _______, _______, _______, _______, _______, _______, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
             _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-            _______, _______, _______, _______, _______, _______, _______,_______, _______, _______, _______, _______
-            ),
+            _______, _______, _______, _______, _______, _______, _______,_______, _______, _______,  _______, _______),
 
     [_RAISE] = LAYOUT_planck_grid(
-            KC_TILD, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10, KC_BSPC,
-            KC_DEL,  KC_F11,  KC_F12,  _______, _______, _______, KC_LEFT, KC_DOWN,  KC_UP,  KC_RGHT,KC_6,    KC_PLUS,
-            _______, _______, _______, _______, _______, _______, _______, _______,  KC_1,   KC_2,   KC_3,    _______,
-            _______, _______, _______, _______, _______, _______, _______, _______,  _______,KC_DOT, KC_EQL,  KC_MPLY
-            ),
+            KC_TILD, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_BSPC,
+            KC_DEL,  KC_F11,  KC_F12,  _______, _______, _______, KC_LEFT, KC_DOWN,  KC_UP,  KC_RGHT, KC_6,    KC_PLUS,
+            _______, _______, _______, _______, _______, _______, _______, _______,  KC_1,   KC_2,    KC_3,    _______,
+            _______, _______, _______, _______, _______, _______, _______, _______,  _______,KC_DOT,  KC_EQL,  KC_MPLY),
 
     [_ADJUST] = LAYOUT_planck_grid(
-            _______, RESET,   AG_SWAP, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD,  RGB_VAI, _______, KC_DEL ,
-            _______, _______, _______, AU_ON,   AU_OFF,  AG_NORM, KC_LEFT, KC_DOWN, KC_UP,    KC_RGHT, _______, _______,
-            _______, _______, _______, _______, _______, _______, _______, AG_NORM, _______,  _______, _______, _______,
-            _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______
-            ),
+            _______, RESET,   AG_SWAP, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, _______, KC_DEL ,
+            _______, _______, _______, AU_ON,   AU_OFF,  AG_NORM, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
+            _______, _______, _______, _______, _______, _______, _______, AG_NORM, _______, _______, _______, _______,
+            _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______),
 
     [_VIM] = LAYOUT_planck_grid(
-            _______, _______, VIM_W,   VIM_E,   VIM_R, _______, VIM_Y,   VIM_U,   VIM_I,    VIM_O,   VIM_P,   _______,
-            VIM_ESC, VIM_A,   VIM_S,   VIM_D,   _______, VIM_G,   VIM_H,   VIM_J,   VIM_K,    VIM_L,   _______, _______,
-            VIM_SHIFT,_______,VIM_X,   VIM_C,   VIM_V,   VIM_B,   _______,_______,VIM_COMMA,VIM_PERIOD,_______,_______,
+            _______, _______, VIM_W,   VIM_E,   VIM_R,   _______, VIM_Y,   VIM_U,   VIM_I,   VIM_O,   VIM_P,   _______,
+            VIM_ESC, VIM_A,   VIM_S,   VIM_D,   _______, VIM_G,   VIM_H,   VIM_J,   VIM_K,   VIM_L,   _______, _______,
+            VIM_SHIFT,_______,VIM_X,   VIM_C,   VIM_V,   VIM_B,   _______,_______,  VIM_COMMA,VIM_PERIOD,_______,_______,
             _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,_______),
 
     [_VINSERT] = LAYOUT_planck_grid(
