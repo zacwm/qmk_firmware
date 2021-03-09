09493a2dec926c001b50fcd84b4500155f170534
Dean Herbert <pe@ppy.sh>
2021-03-09 12:28:44 +0900

    Large changes, not sure if work

--- keyboards/planck/keymaps/ppy/keymap.c
+++ keyboards/planck/keymaps/ppy/keymap.c
@@ -64,15 +64,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_MEH,  KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  KC_SPC,  RAISE,   VIM_START,KVM_SWT,CAP_IMG,   CAP_IMG),
 
     [_LOWER] = LAYOUT_planck_grid(
-            KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-            _______, KC_LPRN, KC_RPRN, _______, _______, KC_LCBR, KC_RCBR, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_ENT,
-            _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_BSLS, _______,
-            _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, CAP_MOV, CAP_MOV),
+            KC_GRV,  _______, _______, _______, _______, KC_LPRN, KC_RPRN, KC_7,    KC_8,    KC_9,    KC_EQL,  _______,
+            _______, _______, _______, _______, _______, KC_LBRC, KC_RBRC, KC_4,    KC_5,    KC_6,    KC_MINS, KC_ENT,
+            _______, _______, _______, _______, _______, KC_LCBR, KC_RCBR, KC_1,    KC_2,    KC_3,    KC_BSLS, _______,
+            _______, _______, _______, _______, _______, _______, _______, KC_0,    KC_DOT,  _______, CAP_MOV, CAP_MOV),
 
     [_RAISE] = LAYOUT_planck_grid(
-            _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_DEL,
-            _______, KC_F11,  KC_F12,  _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, KC_ENT,
-            _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+            _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______, _______, _______, _______, _______, _______, KC_DEL,
+            _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, KC_ENT,
+            _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______, _______, _______, _______, _______,
             _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______),
 
     [_ADJUST] = LAYOUT_planck_grid(
