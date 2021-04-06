1ba235661c7335a862ec3e6732d4996300fc6983
Dean Herbert <pe@ppy.sh>
2021-04-06 17:24:16 +0900

    When on fn/raise, ctrl_esc is always ctrl

--- keyboards/planck/keymaps/ppy/keymap.c
+++ keyboards/planck/keymaps/ppy/keymap.c
@@ -63,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_RAISE] = LAYOUT_planck_grid(
             _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_DEL,
-            _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______, _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______,
+            KC_LCTL, KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______, _______, KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______,
             _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_ENT,
             _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______),
 
