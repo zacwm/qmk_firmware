2eaae6a5320a6762b7893f041b2452c1ca85fefc
Dean Herbert <pe@ppy.sh>
2021-04-08 13:02:28 +0900

    Switch tilde and grave

--- keyboards/planck/keymaps/ppy/keymap.c
+++ keyboards/planck/keymaps/ppy/keymap.c
@@ -57,8 +57,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             MEH,     KC_LCTL, KC_LALT, RAISE,   KC_LGUI, LOWER,   KC_SPC,  KC_ENT,  VIM_START,_______,COPY,    PASTE),
 
     [_LOWER] = LAYOUT_planck_grid(
-            KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-            KC_TILD, KC_LPRN, KC_RPRN, KC_LT,   KC_EQL,  KC_GT,   KC_UNDS, KC_MINS, KC_LBRC, KC_RBRC, KC_SCLN, _______,
+            KC_TILD, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
+            KC_GRV,  KC_LPRN, KC_RPRN, KC_LT,   KC_EQL,  KC_GT,   KC_UNDS, KC_MINS, KC_LBRC, KC_RBRC, KC_SCLN, _______,
             _______, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, _______, KC_BSLS, KC_ENT,
             _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______),
 
