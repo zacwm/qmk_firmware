ce39d439af5ceeb5ef013460c184ff0c23fe7a27
Dean Herbert <pe@ppy.sh>
2021-04-08 13:09:28 +0900

    Move commons out to new file

---
+++ keyboards/planck/keymaps/ppy/common.h
@@ -0,0 +1,41 @@
+#include QMK_KEYBOARD_H
+
+enum planck_layers {
+    _QWERTY,
+    _LOWER,
+    _RAISE,
+    _FKEYS,
+    _ADJUST,
+    _NAV,
+    _VIM,
+    _VINSERT,
+};
+
+enum planck_keycodes {
+    QWERTY = VIM_SAFE_RANGE,
+    BACKLIT,
+    CTRL_ESC,
+    NAV_SCLN,
+    KVM_SWT,
+    COPY,
+    LOWER,
+    RAISE,
+    MEH
+};
+
+#define FKEYS MO(_FKEYS)
+
+#define WORD_L LALT(KC_LEFT)
+#define WORD_R LALT(KC_RIGHT)
+
+#define LINE_L LCTL(KC_LEFT)
+#define LINE_R LCTL(KC_RIGHT)
+
+#define CLOSE_W LGUI(KC_W)
+
+#define PASTE LGUI(KC_V)
+
+#define CAP_IMG LGUI(LSFT(KC_4))        // Capture portion of screen
+#define CAP_MOV LGUI(LSFT(KC_5))        // Capture portion of screen
+
+
--- keyboards/planck/keymaps/ppy/keymap.c
+++ keyboards/planck/keymaps/ppy/keymap.c
@@ -1,43 +1,6 @@
 #include QMK_KEYBOARD_H
 #include "xtonhasvim.h"
-
-enum planck_layers {
-    _QWERTY,
-    _LOWER,
-    _RAISE,
-    _FKEYS,
-    _ADJUST,
-    _NAV,
-    _VIM,
-    _VINSERT,
-};
-
-enum planck_keycodes {
-    QWERTY = VIM_SAFE_RANGE,
-    BACKLIT,
-    CTRL_ESC,
-    NAV_SCLN,
-    KVM_SWT,
-    COPY,
-    LOWER,
-    RAISE,
-    MEH
-};
-
-#define FKEYS MO(_FKEYS)
-
-#define WORD_L LALT(KC_LEFT)
-#define WORD_R LALT(KC_RIGHT)
-
-#define LINE_L LCTL(KC_LEFT)
-#define LINE_R LCTL(KC_RIGHT)
-
-#define CLOSE_W LGUI(KC_W)
-
-#define PASTE LGUI(KC_V)
-
-#define CAP_IMG LGUI(LSFT(KC_4))        // Capture portion of screen
-#define CAP_MOV LGUI(LSFT(KC_5))        // Capture portion of screen
+#include "common.h"
 
 float song_kvm_setting[][2] = SONG(S__NOTE(_C5),S__NOTE(_C6),S__NOTE(_C7));
 
