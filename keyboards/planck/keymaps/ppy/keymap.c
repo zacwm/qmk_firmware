bc3ee5c6e04d6f517b4eec33108c68070df7539f
Dean Herbert <pe@ppy.sh>
2021-06-30 00:47:10 +0900

    Move kvm swt closer

--- keyboards/planck/keymaps/ppy/keymap.c
+++ keyboards/planck/keymaps/ppy/keymap.c
@@ -17,12 +17,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
             CTRL_ESC,KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    NAV_SCLN,KC_QUOT,
             KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-            MEH,     KC_LCTL, KC_LALT, KC_LGUI, FKEYS,   LOWER,   KC_SPC,  KC_ENT,  FKEYS,   KVM_SWT, COPY,    PASTE),
+            MEH,     KC_LCTL, KC_LALT, KC_LGUI, FKEYS,   LOWER,   KC_SPC,  KC_ENT,  KVM_SWT, KVM_SWT, COPY,    PASTE),
 
     [_LOWER] = LAYOUT_planck_grid(
             KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-            KC_TILD, KC_LPRN, KC_RPRN, KC_HASH, KC_EQL,  KC_LT,   KC_GT,   KC_MINS, KC_ASTR, KC_LCBR, KC_RCBR, KC_DQUO,
-            _______, KC_EXLM, KC_AT,   KC_PLUS, KC_DLR,  KC_PERC, KC_CIRC, KC_UNDS, KC_AMPR, KC_LBRC, KC_RBRC, KC_BSLS,
+            KC_TILD, KC_LPRN, KC_RPRN, KC_HASH, KC_EQL,  KC_LT,   KC_GT,   KC_MINS, KC_ASTR, KC_LCBR, KC_RCBR, KC_PIPE,
+            _______, KC_EXLM, KC_AT,   KC_PLUS, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_LBRC, KC_RBRC, KC_BSLS, KC_UNDS,
             _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______),
 
     [_MEH] = LAYOUT_planck_grid(
