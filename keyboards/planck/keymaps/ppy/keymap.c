24d52464b704fb0e7975940f5a34645524066078
Dean Herbert <pe@ppy.sh>
2021-07-31 14:10:55 +0900

    Add more Fkeys to right side

--- keyboards/planck/keymaps/ppy/keymap.c
+++ keyboards/planck/keymaps/ppy/keymap.c
@@ -20,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_FKEYS] = LAYOUT_planck_grid(
             _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   _______, _______, _______, KC_MS_U, _______, KC_WH_U, KC_DEL,
             KC_LCTL, KC_F5,   KC_F6,   KC_F7,   KC_F8,   _______, _______, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D, _______,
-            _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______, _______, _______, _______, _______,
+            _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, KC_F13,  KC_F14,  KC_F15,  KC_F16,  _______,
             _______, _______, _______, _______, _______, _______, _______, KC_BTN1, KC_BTN2, _______, _______, _______),
 
     // Loosely vim-based cursor and document navigation keys.
