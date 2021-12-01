9618bfa18f859538124791870abdefd319397a18
Dean Herbert <pe@ppy.sh>
2021-12-01 19:55:06 +0900

    Bottom row fuck ducks

--- keyboards/planck/keymaps/ppy/keymap.c
+++ keyboards/planck/keymaps/ppy/keymap.c
@@ -7,14 +7,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
             CTRL_ESC,KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    NAV_SCLN,KC_QUOT,
             KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-            MEH,     KC_LCTL, KC_LALT, FKEYS,   KC_LGUI, SYMBOL,  KC_SPC,  KC_ENT,  RMEH,  KVM_SWT, COPYADDR,PASTE),
+            MEH,     KC_LCTL, KC_LALT, KC_LGUI, FKEYS,   SYMBOL,  KC_SPC,  RMEH,    KC_ENT,  KVM_SWT, COPYADDR,PASTE),
 
     // Every symbol required for coding and every-day use.
     [_SYMBOL] = LAYOUT_planck_grid(
             KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
             KC_TILD, KC_LPRN, KC_RPRN, KC_HASH, KC_EQL,  KC_PERC, KC_CIRC, KC_MINS, KC_ASTR, KC_LCBR, KC_RCBR, KC_LBRC,
             KC_PIPE, KC_EXLM, KC_AT,   KC_PLUS, KC_DLR,  KC_PIPE, KC_UNDS, KC_AMPR, KC_LT,   KC_GT,   KC_BSLS, KC_RBRC,
-            _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______),
+            _______, _______, _______, _______, _______, _______, _______, KC_ENT,  _______, _______, _______, _______),
 
     // Function keys, mouse emulation and less commonly used special keys.
     [_FKEYS] = LAYOUT_planck_grid(
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             _______, _______, _______, WORD_R,  _______, _______, _______, KC_PGUP, LINE_R,  LINE_L,  _______, _______,
             _______, _______, _______, KC_PGDN, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______,
             _______, _______, CUT,     COPY,    PASTE,   WORD_L,  _______, _______, _______, _______, _______, _______,
-            _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______),
+            _______, _______, _______, _______, _______, _______, _______, KC_ENT,  _______, _______, _______, _______),
 
     // One-handed gaming layout.
     // Left hand shifted one key further left than usual for WASD usage.
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
             KC_LCTL, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_SCLN, _______,
             _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_LOCK,
-            _______, KC_ESC,  _______, _______, KC_SPC,  _______, _______, _______, _______, _______, XBX_IMG, XBX_MOV),
+            _______, KC_ESC,  _______, _______, KC_SPC,  _______, _______, KC_ENT,  _______, _______, XBX_IMG, XBX_MOV),
 
     // Special macros accessible via MEH key (bottom-left corner key).
     // Undefined keys will get the standard MEH modifiers added.
@@ -370,6 +370,7 @@ void deactivate_meh(uint16_t keycode) {
     unregister_code(KC_LCTL);
     unregister_code(KC_LSFT);
     unregister_code(KC_LALT);
+    unregister_code(KC_ENT);
     layer_off(_MEH);
 
     if (meh_activated == 1)
@@ -379,7 +380,7 @@ void deactivate_meh(uint16_t keycode) {
                 SEND_STRING(SS_LGUI(SS_TAP(X_C)));
                 break;
             case RMEH:
-                SEND_STRING(SS_LSFT(SS_LCTL(SS_LALT(SS_TAP(X_C)))));
+                tap_code16(KC_ENT);
                 break;
         }
     }
@@ -392,8 +393,16 @@ bool process_meh(uint16_t keycode, keyrecord_t *record) {
         case MEH:
         case RMEH:
             if (record->event.pressed) {
-                meh_activated = 1;
-                layer_move(_MEH);
+                if (timer_elapsed(last_key_time) < 250)
+                {
+                    register_code(KC_ENT);
+                    meh_activated = 3;
+                }
+                else
+                {
+                    meh_activated = 1;
+                    layer_move(_MEH);
+                }
             }
             else
                 deactivate_meh(keycode);
